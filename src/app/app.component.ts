92ceca6ae71b2b3abdacb2e25e226d0cba273a77
shmool <shmuelaj@gmail.com>
2020-05-23 14:08:56 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'ngGirlsAngular ' + VERSION.major;
+  name = 'ngGirls!';
 }
